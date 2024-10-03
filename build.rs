2c6e0846e5729fd8903b80c0505ebe4619f6d341
Geoffrey Washburn <geoff@washburght.org>
2024-10-02 23:30:11 -0400

    Disable test incompatible with coverage.  Updated dependencies.

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
-fn main()  {
-    println!("cargo:rustc-link-search=framework={}", "/System/Library/Frameworks");
-    println!("cargo:rustc-link-search=framework={}", "/System/Library/PrivateFrameworks");
+fn main() {
+    println!(
+        "cargo:rustc-link-search=framework={}",
+        "/System/Library/Frameworks"
+    );
+    println!(
+        "cargo:rustc-link-search=framework={}",
+        "/System/Library/PrivateFrameworks"
+    );
 }
--- tests/knoll_tests.rs
+++ tests/knoll_tests.rs
@@ -9,10 +9,13 @@ fn tmp_test() {
     let mut vec_out: Vec<u8> = Vec::new();
     let file_err = tempfile::tempfile().expect("Failed to create temporary file.");
     let args = vec!["knoll", "help"];
+    // Disabled until conflict with coverage checking is resolved.
+    /*
     let _ = run::<RealDisplayState, std::io::Stdin, &mut Vec<u8>, std::fs::File>(
         &args.into_iter().map(|s| String::from(s)).collect(),
         std::io::stdin(),
         &mut vec_out,
         file_err.try_clone().expect("Clone failed"),
     );
+    */
 }
