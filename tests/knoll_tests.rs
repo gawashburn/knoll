eef5127bdc78adef9826fd273a70a4f5bce4432d
Geoffrey Washburn <geoff@washburght.org>
2024-11-30 16:18:12 -0500

    Cleanup and expansion of whole program tests

--- tests/knoll_tests.rs
+++ tests/knoll_tests.rs
@@ -6,28 +6,33 @@ use knoll::knoll::run;
 use knoll::real_displays::*;
 use tempfile;
 
-#[test]
-fn test_help() {
+#[cfg_attr(all(coverage_nightly, test), coverage(off))]
+fn run_knoll(args: Vec<&str>) -> (String, String) {
     let mut vec_out: Vec<u8> = Vec::new();
     let file_err = tempfile::tempfile().expect("Failed to create temporary file.");
-    let args = vec!["knoll", "help"];
     let _ = run::<RealDisplayState, std::io::Stdin, &mut Vec<u8>, std::fs::File>(
         &args.into_iter().map(|s| String::from(s)).collect(),
         std::io::stdin(),
         &mut vec_out,
         file_err.try_clone().expect("Clone failed"),
     );
+
+    (
+        String::from_utf8(vec_out).unwrap(),
+        String::from("".to_string()),
+    )
+}
+#[test]
+fn test_default() {
+    run_knoll(vec!["knoll"]);
+}
+
+#[test]
+fn test_help() {
+    run_knoll(vec!["knoll", "help"]);
 }
 
 #[test]
 fn test_list() {
-    let mut vec_out: Vec<u8> = Vec::new();
-    let file_err = tempfile::tempfile().expect("Failed to create temporary file.");
-    let args = vec!["knoll", "list"];
-    let _ = run::<RealDisplayState, std::io::Stdin, &mut Vec<u8>, std::fs::File>(
-        &args.into_iter().map(|s| String::from(s)).collect(),
-        std::io::stdin(),
-        &mut vec_out,
-        file_err.try_clone().expect("Clone failed"),
-    );
+    run_knoll(vec!["knoll", "list"]);
 }
